44c536e29556bc73b345e668e2aba372d0f125e4
0230cp <rhwocks69@gmail.com>
2022-08-25 23:41:04 +0900

    Initial commit

---
+++ src/main/java/cp/Traduler/TradulerApplication.java
@@ -0,0 +1,13 @@
+package cp.Traduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradulerApplication.class, args);
+	}
+
+}
