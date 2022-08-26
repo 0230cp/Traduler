ada7ebf5fc97797d4fb649efefb8297bf88dee20
rnjsgo <rnjsgo2008@naver.com>
2022-08-26 15:59:07 +0900

    login화면 수정중

---
+++ src/main/java/cp/Traduler/controller/LoginController.java
@@ -0,0 +1,14 @@
+package cp.Traduler.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class LoginController {
+    @GetMapping("/login")
+    public String main(){
+        return "login";
+    }
+}
