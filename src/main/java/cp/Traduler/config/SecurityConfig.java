1f2a6e8a5d0c7c3a25f4f8b960d51b6f3b7b979b
rnjsgo <rnjsgo2008@naver.com>
2022-08-27 17:18:47 +0900

    SecurityConfig 수정

--- src/main/java/cp/Traduler/config/SecurityConfig.java
+++ src/main/java/cp/Traduler/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package cp.Traduler.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +14,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER") .and()
+                .withUser("admin").password("{noop}password").roles("ADMIN");
+    }
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
     }
 
     @Bean
